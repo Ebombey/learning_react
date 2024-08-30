41910a8d2f22a4014f56220f600788f4340126e1
Ebombey <naveenraikwar200@gmail.com>
2024-08-30 19:35:47 +0530

    add dynamic notes

--- 06keeper/src/App.jsx
+++ 06keeper/src/App.jsx
@@ -3,12 +3,20 @@ import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Note from "./components/Note";
+import notes from "./notes";
 
 function App() {
+  console.log(notes);
   return (
     <div className="w-full h-screen bg-black flex flex-col">
       <Header />
-      <Note />
+      <div className="flex flex-grow">
+        {notes.map((note) => {
+          return (
+            <Note key={note.key} title={note.title} content={note.content} />
+          );
+        })}
+      </div>
       <Footer />
     </div>
   );
--- 06keeper/src/components/Note.jsx
+++ 06keeper/src/components/Note.jsx
@@ -1,14 +1,20 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Note() {
+function Note({ title, content }) {
   return (
     <>
-      <div className="text-black bg-[#fff9e3] w-60 m-6 p-3 border rounded-md flex flex-col gap-2">
-        <h1 className="text-xl font-bold">This is the note title</h1>
-        <p className="text-lg">This is the note content</p>
+      <div className="text-black bg-[#fff9e3] w-60 max-h-64 m-6 p-3 border rounded-md flex flex-col gap-2">
+        <h1 className="text-xl font-bold">{title}</h1>
+        <p className="text-lg">{content}</p>
       </div>
     </>
   );
 }
+
+Note.propTypes = {
+  title: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
+};
 
 export default Note;
