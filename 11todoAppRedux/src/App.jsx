cbd7a9fe0600bd0ba515ba57a5563569ef8dfa46
Ebombey <naveenraikwar200@gmail.com>
2024-09-22 17:43:16 +0530

    redus todo

---
+++ 11todoAppRedux/src/App.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import "./App.css";
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todo";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+
+function App() {
+  return (
+    <Provider store={store}>
+      <AddTodo />
+      <Todos />
+    </Provider>
+  );
+}
+
+export default App;
